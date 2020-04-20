0867559474ede2edd811b0579359a92eb9861902
varunraj <varunraj82@gmail.com>
2020-04-20 10:24:41 -0400

    working of hello world

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// create a new component
+// create a new component. Component is a class
 
 const App = function(){
   return <div>Hi</div>
 }
 
-// Deliver the component to DOM
+// Deliver the component instance (from class created) to DOM. We need to place the app inside 
+// div container in main page
 
-React.render(App);
+ReactDOM.render(<App></App>, document.querySelector('.container'));
 
