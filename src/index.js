2cbef300a0f53de5a077342cd24d6053ae6bc37b
varunraj <varunraj82@gmail.com>
2020-04-20 12:33:14 -0400

    created search bar and added to main page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { config } from '../config';
+import SearchBar from './components/search_bar'
 
 const API_KEY = config.youtube_key
 
 // create a new component. Component is a class
 
 const App = () => {
-  return <div>Hi</div>
+  return (
+        <div>
+          <SearchBar />
+        </div>
+  );
 }
 
 // Deliver the component instance (from class created) to DOM. We need to place the app inside 
