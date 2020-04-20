702b3f524dd1afaff49106d5681ec9680a07dc3e
varunraj <varunraj82@gmail.com>
2020-04-20 16:05:20 -0400

    updated search bar to set the state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,11 +3,27 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
+    // define constructor to instantiate state
+
+    constructor(props) {
+        super(props);
+        this.state = { term: 'Enter search'};
+    
+    }
+
     // override the render function from baseclass
 
     render()   {
-        return <input />;
+        return (
+            <div>     
+                <input
+                    value = {this.state.term} 
+                    onChange = {event => this.setState( { term : event.target.value })} />
+            </div>
+        );
     }
+
+
 
 }
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { config } from '../config';
 import SearchBar from './components/search_bar'
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = config.youtube_key
+
+YTSearch({key:API_KEY, term:'surfboards'}, function(response){
+  console.log(response);
+});
+
 
 // create a new component. Component is a class
 
