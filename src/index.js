b7cdb813ec07af0bc2263641af01d1b9bbaa5a0d
varunraj <varunraj82@gmail.com>
2020-04-21 12:38:52 -0400

    updated app to class based component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import { config } from '../config';
 import SearchBar from './components/search_bar'
@@ -6,19 +6,29 @@ import YTSearch from 'youtube-api-search';
 
 const API_KEY = config.youtube_key
 
-YTSearch({key:API_KEY, term:'surfboards'}, function(response){
-  console.log(response);
-});
+
 
 
 // create a new component. Component is a class
 
-const App = () => {
-  return (
-        <div>
-          <SearchBar />
-        </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {videos:[]};
+
+    YTSearch({key:API_KEY, term:'surfboards'}, (videos) => {
+      this.setState({ videos }) ;
+    });
+
+  }
+
+  render(){
+    return (
+          <div>
+            <SearchBar />
+          </div>
+    )}
 }
 
 // Deliver the component instance (from class created) to DOM. We need to place the app inside 
