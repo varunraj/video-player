0e14e3b6a36c183c4cb550fabf685b2556faf149
varunraj <varunraj82@gmail.com>
2020-04-22 10:09:35 -0400

    update video player with selected video from list

--- src/index.js
+++ src/index.js
@@ -18,10 +18,17 @@ class App extends Component {
 
   constructor(props){
     super(props);
-    this.state = {videos:[]};
+    this.state = {
+      videos:[],
+      selectedVideo : null
+    };
 
     YTSearch({key:API_KEY, term:'surfboards'}, (videos) => {
-      this.setState({ videos }) ;
+      this.setState({
+         videos,
+         selectedVideo: videos[1] 
+        
+        }) ;
     });
 
   }
@@ -30,8 +37,10 @@ class App extends Component {
     return (
           <div>
             <SearchBar />
-            <VideoDetail video = {this.state.videos[1]}/>
-            <VideoList videos = {this.state.videos} />
+            <VideoDetail video = {this.state.selectedVideo}/>
+            <VideoList
+              onVideoSelect = {selectedVideo => this.setState({selectedVideo})} 
+              videos = {this.state.videos} />
           </div>
     )}
 }
